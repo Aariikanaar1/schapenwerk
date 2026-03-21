4e75f911caf9cbac859d237067323cb6312d617e
Aariikanaar1 <aariikanaar1@proton.me>
2026-03-21 22:52:32 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,32 +1,18 @@
-const CACHE_NAME = 'schapenwerk-v4.0';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-];
+const CACHE_NAME = 'schapenwerk-v1';
+const urlsToCache = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
   self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => {
+    if (key !== CACHE_NAME) return caches.delete(key);
+  }))));
   self.clients.claim();
 });
