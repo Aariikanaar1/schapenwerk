2ed708b944bd379be4ebb3093c054b790b47325a
Aariikanaar1 <aariikanaar1@proton.me>
2026-03-21 22:42:56 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,23 +1,22 @@
-const CACHE_NAME = 'schapenwerk-v3.9';
+const CACHE_NAME = 'schapenwerk-v4.0';
 const urlsToCache = [
   '/',
   '/index.html',
-  'https://unpkg.com/leaflet@1.9.3/dist/leaflet.css',
-  'https://unpkg.com/leaflet@1.9.3/dist/leaflet.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'
+  '/manifest.json'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
 
@@ -25,9 +24,9 @@ self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
       );
     })
   );
+  self.clients.claim();
 });
